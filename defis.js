0272a343fd6bf52470de1caec7ca2aad922e4016
JulieDavoine <julie4793@hotmail.fr>
2017-10-19 15:42:56 +0200

    carré vert

--- defis.js
+++ defis.js
@@ -1,4 +1,11 @@
-function chgColor (div)
-{
-    document.getElementById(ladiv).style.backgroundColor='#bab9b7';
-}
+function chgColor (){
+    
+
+   document.getElementById("ladiv").style.backgroundColor='green';
+
+   }
+
+   var button = document.getElementById("lebouton");
+   button.onClick = function() {
+       
+   }
